8b8614045259b90a3edc8bee3d4b14b714ca63f7
Ben Tien <benjamin.tien@flatironschool.com>
2019-01-05 23:31:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,11 +4,15 @@
 def key_for_min_value(name_hash)
     min_value = 100000
     min_key = ""
-    name_hash.collect do |key, value|
+    if name_hash.size == 0
+      return nil
+    else 
+      name_hash.collect do |key, value|
       if value < min_value
         min_value = value
         min_key = key
       end
     end
+  end
     min_key
 end
